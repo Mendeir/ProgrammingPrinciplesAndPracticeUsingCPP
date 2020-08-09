2d87f8677677358d02cdb9a39303ae08f1571243
Adrianne Magracia <adriannemagracia.git@gmail.com>
2020-08-09 18:10:04 +0800

    Initial Commit

---
+++ ProgrammingPrinciplesAndPracticeUsingCPP/Chapter05/TryThisTypeErrors.cpp
@@ -0,0 +1,11 @@
+#include "std_lib_facilities.h"
+
+int main ()
+{
+	int x0 = arena (7);                    // error: undeclared function
+	int x1 = area (7);                      // error: wrong number of arguments
+	int x2 = area ("seven", 2);      // error: 1st argument has a wrong type
+
+
+	return 0;
+}
